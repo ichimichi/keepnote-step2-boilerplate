ec476062ff7f2147d373190e8299980a31ee1b71
Laribok.Syiemlieh <laribok.syiemlieh@ibm.com>
2021-03-06 16:12:49 +0530

    updated username

--- src/main/java/com/stackroute/keepnote/config/ApplicationContextConfig.java
+++ src/main/java/com/stackroute/keepnote/config/ApplicationContextConfig.java
@@ -43,9 +43,9 @@ public class ApplicationContextConfig {
 	public DataSource getDataSource() {
 		BasicDataSource dataSource = new BasicDataSource();
 		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://" +  System.getenv("MYSQL_HOST") + ":3306/" + System.getenv("MYSQL_DATABASE")ww
+		dataSource.setUrl("jdbc:mysql://" +  System.getenv("MYSQL_HOST") + ":3306/" + System.getenv("MYSQL_DATABASE")
 		+ "verifyServerCertificate=false&useSSL=false&requireSSL=false");
-		dataSource.setUsername("root");
+		dataSource.setUsername(System.getenv("MYSQL_USER"));
 		dataSource.setPassword(System.getenv("MYSQL_PASSWORD"));
 		return dataSource;
 	}
